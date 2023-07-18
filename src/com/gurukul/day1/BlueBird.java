b780bcbc51112030ed03afdc182cce0a2620399b
Rupendrarupedit <rupendradhawas231@gmail.com>
2023-07-18 11:12:00 +0530

    changes in the code

--- src/com/gurukul/day1/BlueBird.java
+++ src/com/gurukul/day1/BlueBird.java
@@ -6,10 +6,11 @@ public class BlueBird {
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter the number of people travelling");
+		System.out.println("Enter the total number of Passengers");
 		int Ppl = sc.nextInt();
-		int Tp= 50*(Math.floorDiv(Ppl, 50));
-		System.out.println("the No.of passengers Travelling "+ Tp);
+		int TrB = Ppl/50; //to find the no. of bus required
+		int Tp= 50*TrB;
+		System.out.println("the No.of passengers actually Travelling "+ Tp);
 		
 
 	}
