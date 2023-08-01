0ff4331976142d4ad2c1427f4484ee563c263132
Rupendrarupedit <127604394+Rupendrarupedit@users.noreply.github.com>
2023-08-02 00:39:54 +0530

    projectWork

---
+++ src/ProjectMusicRecommender/MusicFileFormatException.java
@@ -0,0 +1,20 @@
+package ProjectMusicRecommender;
+
+public class MusicFileFormatException extends Exception
+{
+    @Override
+	public String toString()
+	{
+		return "file extention is incorrect";
+	}
+}
+
+class NoRecommendationException extends Exception
+{
+    @Override
+	public String toString()
+	{
+		return "Oops !!! No recomendation found...";
+	}
+}
+
